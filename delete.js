a0b9508a8015c3c1d2ce803d5636ec4f70e9d9e8
olususi oluyemi <yemiwebby@gmail.com>
2022-01-25 14:31:20 +0100

    more updates

---
+++ delete.js
@@ -0,0 +1,22 @@
+const axios = require("axios").default;
+require("dotenv").config();
+
+const API_BASE_URL = "https://circleci.com/api/v2/schedule";
+const vcs = process.env.VCS_TYPE;
+const org = process.env.ORG_NAME;
+const project = process.env.PROJECT_ID;
+const token = process.env.CIRCLECI_TOKEN;
+
+const schedule_ids = ["7b7dc429-fea4-42d5-8e36-328b7bd98e32"];
+
+async function deleteScheduleById() {
+  for (let i = 0; i < schedule_ids.length; i++) {
+    let deleteScheduleEndpoint = `${API_BASE_URL}/${schedule_ids[i]}`;
+    let res = await axios.delete(deleteScheduleEndpoint, {
+      headers: { "circle-token": token },
+    });
+    console.log(res.data);
+  }
+}
+
+deleteScheduleById();
